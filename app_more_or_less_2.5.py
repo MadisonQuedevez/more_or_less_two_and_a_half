ae7046e6e398e8b595ac6a99a09777161b3ae728
Madison Quedevez <MadisonQuedevez@gmail.com>
2023-07-06 13:25:02 -0300

    retired setup locale

--- app_more_or_less_2.5.py
+++ app_more_or_less_2.5.py
@@ -7,20 +7,6 @@ st.markdown("<h1 style='text-align: center;'>More or Less: 2.5 Goals</h1>", unsa
 st.markdown("<h2 style='text-align: center;'>Um app para estimativa e separação dos jogos</h2>", unsafe_allow_html=True)
 
 
-def setup_locale():
-    # Definir a configuração regional para o formato brasileiro
-    locale.setlocale(locale.LC_ALL, 'pt_BR.utf8')
-
-def main():
-    setup_locale()
-
-    # Resto do seu código Streamlit
-    data = st.date_input("Selecione uma data")
-
-if __name__ == '__main__':
-    main()
-
-
 """
 
 ---
