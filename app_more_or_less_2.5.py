92af8d3a925d25b74bd74fc8d0ba33eb681ca1c5
Madison Quedevez <MadisonQuedevez@gmail.com>
2023-07-06 16:53:07 -0300

    corrindo erro no redimensionamento

--- app_more_or_less_2.5.py
+++ app_more_or_less_2.5.py
@@ -1,13 +1,18 @@
 import streamlit as st
 from datetime import date
+from ipywidgets import DatePicker
 
 
 st.markdown("<h2 style='text-align: center;'>More or Less: 2.5 Goals</h2>", unsafe_allow_html=True)
 st.markdown("<h4 style='text-align: center;'>Um app para estimativa e separação dos jogos</h4>", unsafe_allow_html=True)
 st.divider()
 
-# Criando a caixa de seleção de data com largura e altura personalizadas
-data = st.date_input("Selecione uma data", date.today(), width=150, height=50)
+# Criando a caixa de seleção de data personalizada com largura e altura
+date_select = DatePicker(value=date.today(), layout={'width': '150px', 'height': '50px'})
+
+# Exibindo o widget no Streamlit
+st.write(date_select)
+
 
 
 """
