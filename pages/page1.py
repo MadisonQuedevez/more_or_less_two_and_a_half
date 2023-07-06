d3a4498b7469d01431fd272f5461bdd9ac89e20e
Madison Quedevez <MadisonQuedevez@gmail.com>
2023-07-06 18:15:43 -0300

    alteração no código da page1

--- pages/page1.py
+++ pages/page1.py
@@ -1,13 +1,9 @@
 import streamlit as st
 import requests
-from datetime import date
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-#criando o contéudo da página principal
-st.markdown("<h1 style='text-align: center;'>Backtesting</h1>", unsafe_allow_html=True)
-
+# Função para carregar a base de dados (com cache)
 @st.cache(allow_output_mutation=True)
 def load_base():
     url = "https://github.com/futpythontrader/YouTube/blob/main/Base_de_Dados/futpythontraderpunter.csv?raw=true"
@@ -15,53 +11,44 @@ def load_base():
 
     return data_jogos
 
+# Carregar a base de dados
 df = load_base()
 
-df.loc[(df['FT_Goals_H'] >  df['FT_Goals_A']), 'Profit'] = df['FT_Odd_H'] - 1
-df.loc[(df['FT_Goals_H'] <= df['FT_Goals_A']), 'Profit'] = -1
-
-# Caixa de texto para filtrar Odd Home
+# Filtros
 min_H = st.sidebar.number_input("FT_Odd_H", min_value=1.01, max_value=10.0, value=1.01)
 max_H = st.sidebar.number_input("FT_Odd_H", min_value=1.01, max_value=10.0, value=10.0)
 
-# Caixa de texto para filtrar Odd Draw
 min_D = st.sidebar.number_input("FT_Odd_D", min_value=1.01, max_value=10.0, value=1.01)
 max_D = st.sidebar.number_input("FT_Odd_D", min_value=1.01, max_value=10.0, value=10.0)
 
-# Caixa de texto para filtrar Odd Away
 min_A = st.sidebar.number_input("FT_Odd_A", min_value=1.01, max_value=10.0, value=1.01)
 max_A = st.sidebar.number_input("FT_Odd_A", min_value=1.01, max_value=10.0, value=10.0)
 
-# Caixa de texto para filtrar Odd Over 25
 min_Over25 = st.sidebar.number_input("FT_Odd_Over25", min_value=1.01, max_value=10.0, value=1.01)
 max_Over25 = st.sidebar.number_input("FT_Odd_Over25", min_value=1.01, max_value=10.0, value=10.0)
 
-# Caixa de texto para filtrar Odd BTTS
 min_BTTS = st.sidebar.number_input("FT_Odd_BTTS_Yes", min_value=1.01, max_value=10.0, value=1.01)
 max_BTTS = st.sidebar.number_input("FT_Odd_BTTS_Yes", min_value=1.01, max_value=10.0, value=10.0)
 
-# Filtre o dataframe pelos valores mínimos e máximos de cada coluna
+# Filtrar o dataframe
 df_filtrado = df[(df['FT_Odd_H'] >= min_H) & (df['FT_Odd_H'] <= max_H) &
                  (df['FT_Odd_D'] >= min_D) & (df['FT_Odd_D'] <= max_D) &
                  (df['FT_Odd_A'] >= min_A) & (df['FT_Odd_A'] <= max_A) &
                  (df['FT_Odd_Over25'] >= min_Over25) & (df['FT_Odd_Over25'] <= max_Over25) &
                  (df['FT_Odd_BTTS_Yes'] >= min_BTTS) & (df['FT_Odd_BTTS_Yes'] <= max_BTTS)]
 
-# Crie uma nova coluna no dataframe filtrado com o profit acumulado
-df_filtrado['Profit_acu'] = df_filtrado.Profit.cumsum()
+# Calcular o profit acumulado
+df_filtrado['Profit_acu'] = df_filtrado['Profit'].cumsum()
 df_filtrado = df_filtrado.dropna()
 df_filtrado = df_filtrado.reset_index(drop=True)
 df_filtrado.index += 1
-profit = round(df_filtrado.Profit_acu.tail(1).item(),2)
-ROI = round((df_filtrado.Profit_acu.tail(1)/len(df_filtrado)*100).item(),2)
-df_filtrado.Profit_acu.plot(title="Back Home", xlabel='Entradas', ylabel='Stakes')
-print("Profit:",profit,"stakes em", len(df_filtrado),"jogos")
-print("ROI:",ROI,"%")
+profit = round(df_filtrado['Profit_acu'].tail(1).item(), 2)
+ROI = round((df_filtrado['Profit_acu'].tail(1) / len(df_filtrado) * 100).item(), 2)
 
-# Plote o gráfico com o profit acumulado do dataframe filtrado
-plt.plot(df_filtrado['Profit_acu'])
-st.pyplot()
-st.set_option('deprecation.showPyplotGlobalUse', False)
+# Exibir o gráfico com o profit acumulado
+st.line_chart(df_filtrado['Profit_acu'])
+st.write("Profit:", profit, "stakes em", len(df_filtrado), "jogos")
+st.write("ROI:", ROI, "%")
 
 """
 
