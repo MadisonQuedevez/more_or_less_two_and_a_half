bcf88c238e17415e9c35da993ec43c6e7b1ee6ea
Madison Quedevez <MadisonQuedevez@gmail.com>
2023-07-06 17:56:19 -0300

    criando a lista de competições embutida dentro da lista de áreas

--- app_more_or_less_2.5.py
+++ app_more_or_less_2.5.py
@@ -8,29 +8,30 @@ st.markdown("<h4 style='text-align: center;'>Um app para estimativa e separaçã
 st.divider()
 
 
-# Criando a caixa de seleção de data
-data = st.date_input("Selecione uma data", date.today())
+# Fazendo uma solicitação GET para o endpoint de áreas
+response_areas = requests.get('https://api.sportsdata.io/v4/soccer/scores/json/Areas?key=594352b9547d44d698c51b234bf5f24f')
 
-# Fazendo uma solicitação GET para o endpoint
-response = requests.get('https://api.sportsdata.io/v4/soccer/scores/json/Areas?key=594352b9547d44d698c51b234bf5f24f')
-
-# Analisando a resposta JSON
-data = response.json()
+# Analisando a resposta JSON de áreas
+data_areas = response_areas.json()
 
 # Extraindo a lista de áreas
-areas = [area['Name'] for area in data]
+areas = [area['Name'] for area in data_areas]
 
 # Criando um seletor para as áreas
 selected_area = st.selectbox('Selecione uma área', areas)
 
+# Obtendo a lista de competições correspondente à área selecionada
+competitions = [area['Competition'] for area in data_areas if area['Name'] == selected_area]
+selected_competition = st.selectbox('Selecione uma competição', competitions)
+
 # Endpoint da API
 endpoint = "https://api.sportsdata.io/v4/soccer/stats/json/BoxScoresByDate/{competition}/{date}?key=594352b9547d44d698c51b234bf5f24f"
 
-# Dados de exemplo
-competition = "example"  # Substitua "example" pelo nome da competição desejada
+# Filtros
+filtro_data = st.date_input("Selecione uma data", value=date.today())
 
 # Montando a URL do endpoint com os parâmetros
-url = endpoint.format(competition=competition, date=data)
+url = endpoint.format(competition=selected_competition, date=filtro_data)
 
 # Fazendo a requisição HTTP
 response = requests.get(url)
