2db1dcfd5250cee37d3dc410845c46a439f4ba5a
Madison Quedevez <MadisonQuedevez@gmail.com>
2023-07-06 13:43:33 -0300

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -2,9 +2,6 @@ import streamlit as st
 import datetime
 import locale
 
-# Criando a barra lateral
-st.sidebar.title("Menu Principal")
-st.sidebar.write("Escolha a categoria de gols")
 
 #criando o contéudo da página principal
 st.markdown("<h1 style='text-align: center;'>More or Less: 2.5 Goals</h1>", unsafe_allow_html=True)
