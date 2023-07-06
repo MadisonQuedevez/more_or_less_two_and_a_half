ad582664b46767d555a1c0c14ed08f4f80f7e51f
Madison Quedevez <MadisonQuedevez@gmail.com>
2023-07-06 13:15:54 -0300

    Create app more or less goals 2,5.1.0

---
+++ pages/app_more_or_less_2.5.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import datetime
+import locale
+
+st.markdown("<h1 style='text-align: center;'>More or Less: 2.5 Goals</h1>", unsafe_allow_html=True)
+
+st.markdown("<h2 style='text-align: center;'>Um app para estimativa e separação dos jogos</h2>", unsafe_allow_html=True)
