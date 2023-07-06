51694645182b0f969386d83a729a29208e317fc4
Madison Quedevez <MadisonQuedevez@gmail.com>
2023-07-06 10:21:40 -0300

    teste de título

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,3 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import streamlit as st\n",
-    "\n",
-    "st.title('More or Less +2.5')\n"
-   ]
-  }
- ],
- "metadata": {
-  "language_info": {
-   "name": "python"
-  },
-  "orig_nbformat": 4
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+import streamlit as st 
+
+st.set_page_config(page_title='More or Less 2.5 Gols')
