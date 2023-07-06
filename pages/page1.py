e43f1bc4c71e6489e39e622814134a6172f45f4a
Madison Quedevez <MadisonQuedevez@gmail.com>
2023-07-06 13:38:13 -0300

    nome da barra lateral

--- pages/page1.py
+++ pages/page1.py
@@ -2,9 +2,15 @@ import streamlit as st
 import datetime
 import locale
 
-st.markdown("<h1 style='text-align: center;'>More or Less: 2.5 Goals</h1>", unsafe_allow_html=True)
+# Criando a barra lateral
+st.sidebar.title("Menu Principal")
+st.sidebar.write("Escolha a categoria de gols")
 
+#criando o contéudo da página principal
+st.markdown("<h1 style='text-align: center;'>More or Less: 2.5 Goals</h1>", unsafe_allow_html=True)
 st.markdown("<h2 style='text-align: center;'>Um app para estimativa e separação dos jogos</h2>", unsafe_allow_html=True)
+
+
 
 
 """
