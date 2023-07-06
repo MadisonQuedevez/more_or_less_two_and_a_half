782b8ff53c76df7fc659fcff1abb6e094bb17e9f
Madison Quedevez <MadisonQuedevez@gmail.com>
2023-07-06 16:56:46 -0300

    update data input

--- app_more_or_less_2.5.py
+++ app_more_or_less_2.5.py
@@ -1,17 +1,22 @@
+pip install streamlit-datepicker
+
+
 import streamlit as st
 from datetime import date
-from ipywidgets import DatePicker
+from streamlit_datepicker import st_date_input
 
 
 st.markdown("<h2 style='text-align: center;'>More or Less: 2.5 Goals</h2>", unsafe_allow_html=True)
 st.markdown("<h4 style='text-align: center;'>Um app para estimativa e separação dos jogos</h4>", unsafe_allow_html=True)
 st.divider()
 
-# Criando a caixa de seleção de data personalizada com largura e altura
-date_select = DatePicker(value=date.today(), layout={'width': '150px', 'height': '50px'})
 
-# Exibindo o widget no Streamlit
-st.write(date_select)
+# Criando a caixa de seleção de data com largura e altura personalizadas
+data = st_date_input("Selecione uma data", date.today(), key="data", width=150, height=50)
+
+# Exibindo a data selecionada
+st.write("Data selecionada:", data)
+
 
 
 
