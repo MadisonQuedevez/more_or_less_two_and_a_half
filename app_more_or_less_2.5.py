35368b10fde45bf3175149ba8d86cc22d97a7997
Madison Quedevez <MadisonQuedevez@gmail.com>
2023-07-06 16:39:56 -0300

    create box date selection

--- app_more_or_less_2.5.py
+++ app_more_or_less_2.5.py
@@ -1,10 +1,16 @@
 import streamlit as st
-import datetime
-import locale
+from datetime import date
+
 
 st.markdown("<h2 style='text-align: center;'>More or Less: 2.5 Goals</h2>", unsafe_allow_html=True)
 st.markdown("<h4 style='text-align: center;'>Um app para estimativa e separação dos jogos</h4>", unsafe_allow_html=True)
 st.divider()
+
+# Criando a caixa de seleção de data
+data = st.date_input("Selecione uma data", date.today())
+
+# Exibindo a data selecionada
+st.write("Você selecionou a data:", data)
 
 
 """
